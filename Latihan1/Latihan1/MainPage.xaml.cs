696f5e635fa80c776a4bbf933ce624518a34137c
Erick Kurniawan <erick.kurniawan@gmail.com>
2018-02-06 11:59:54 +0700

    Add project files.

---
+++ Latihan1/Latihan1/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Latihan1
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+            btnTambah.Clicked += BtnTambah_Clicked;
+		}
+
+        private void BtnTambah_Clicked(object sender, EventArgs e)
+        {
+
+            DisplayAlert("Keterangan", "Tombol Tambah Dipilih !", "OK");
+        }
+    }
+}
