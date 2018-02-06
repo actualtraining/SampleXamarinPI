655f83bad4d621d4aab1f424045b06b162086a97
Erick Kurniawan <erick.kurniawan@gmail.com>
2018-02-06 15:04:08 +0700

    menambahkan view model

---
+++ Latihan1/Latihan1/Berita.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Latihan1
+{
+    public class Berita
+    {
+        public string Judul { get; set; }
+        public string Keterangan { get; set; }
+    }
+}
